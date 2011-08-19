a2fa9625894dda04d58e5620efa7fd0315d8b7f7
Gordon <gordon+git@gordonkoo.com>
2011-08-19 13:15:31 -0700

    added profile pictures, profile links, etc

--- linkedin_wvmx.user.js
+++ linkedin_wvmx.user.js
@@ -8,6 +8,9 @@
 
 // include jquery
 // http://erikvold.com/blog/index.cfm/2010/6/14/using-jquery-with-a-user-script
+//
+// get a profile using PAL API
+// http://www.linkedin.com/v1/people/id=14997818:(first-name,last-name,headline,picture-url)
 
 addJQuery = function (callback) {
   var script = document.createElement("script");
@@ -22,21 +25,36 @@ addJQuery = function (callback) {
 
 // This function is called after jquery is initialized.
 addJQuery(function() {
-  var wvmpModule = $('#extra').children('.stats.profile'),
-      link = $('<a>').text('Get notifications when someone views your profile!')
-                     .attr('href', '#'),
-      lastDate = (new Date()).getTime(),
-      WVMX_TIME_INTERVAL = 10000;
+
+  var wvmpModule  = $('#extra').children('.stats.profile'),
+      wvmpSetting = $('<p>').css('padding-top', '10px'),
+      wvmpCheck   = $('<input>').attr({ 'type': 'checkbox',
+                                        'name': 'wvmpCheck',
+                                        'id': 'wvmpCheck' })
+                                .css('float', 'left'),
+      lastDate    = (new Date()).getTime(),
+      doNotify    = false,
+
+      LI_API_PREFIX = 'http://www.linkedin.com/v1/people/',
+      WVMX_TIME_INTERVAL = 10000,
 
 
 
 
-  showNotification = function(logo, headline, body) {
+  showNotification = function(logoUrl, name, profileUrl) {
+    var url;
     if (window.webkitNotifications.checkPermission() !== 0) {
       window.webkitNotifications.requestPermission();
     }
     else {
-      window.webkitNotifications.createNotification(logo, headline, body).show();
+      profileUrl = encodeURIComponent(profileUrl);
+      logoUrl = encodeURIComponent(logoUrl);
+      name = encodeURIComponent(name);
+      url = ['http://localhost:8080/wvmx', name];
+      if (profileUrl) { url.push(encodeURIComponent(profileUrl)) }
+      if (logoUrl) { url.push(encodeURIComponent(logoUrl)) }
+
+      window.webkitNotifications.createHTMLNotification(url.join('/')).show();
     }
   },
 
@@ -47,9 +65,12 @@ addJQuery(function() {
   handleView = function(view, lastDate) {
     var view = $(view),
         time = parseInt(view.find('timestamp').text(), 10),
-        privacyType, viewer, name;
+        privacyType, viewer, name, pictureUrl, profileUrl;
 
     if (time < lastDate) {
+      // old view, discard.
+      // comment out this return statement if you just want to
+      // see example notifications.
       return;
     }
     // new view!
@@ -58,11 +79,49 @@ addJQuery(function() {
     if (privacyType === 'public') {
       viewer = view.find('viewer');
       name = [view.find('first-name').text(), view.find('last-name').text()].join(' ');
+      id = viewer.find('id');
+      if (id && id.length) {
+        // make a call to get the picture url
+        id = id.text();
+        $.get([LI_API_PREFIX, 'id=', parseInt(id, 10), ':(picture-urls::(40x40),site-standard-profile-request)'].join(''), function(data) {
+          pictureUrl = $(data).find('picture-url');
+          profileUrl = $(data).find('site-standard-profile-request');
+
+          // PICTURE URL
+          if (pictureUrl && pictureUrl.length) {
+            // we have a picture
+            pictureUrl = pictureUrl.text();
+          }
+          else {
+            // no picture
+            pictureUrl = '';
+          }
+
+          // PROFILE URL
+          if (profileUrl && profileUrl.length && profileUrl.find('url').length) {
+            profileUrl = profileUrl.find('url');
+            if (profileUrl && profileUrl.length) {
+              profileUrl = profileUrl.text();
+            }
+          }
+          else {
+            profileUrl = ''
+          }
+          showNotification(pictureUrl, name, profileUrl);
+          // we're done!
+        });
+        return; // we're waiting for pictureUrl to come back asynchronously, return for now.
+      }
+    }
+    else if (privacyType === 'obfuscated') {
+      // OBFUSCATED
+      name = view.find('viewer-headline').text();
     }
     else {
+      // HIDDEN
       name = 'Someone';
     }
-    showNotification('', [name, 'viewed your profile!'].join(' '), 'Go check \'em out!');
+    showNotification('', name, '');
   };
 
 
@@ -70,18 +129,40 @@ addJQuery(function() {
 
 
 
-  link.click(function(evt) {
-    if (window.webkitNotifications.checkPermission() !== 0) {
-      window.webkitNotifications.requestPermission();
+  wvmpSetting.prepend(wvmpCheck)
+             .append($('<label>').text('Get notifications when someone views my profile')
+                                 .attr('for', 'wvmpCheck')
+                                 .css({'font-size': '13px',
+                                       'padding-left': '20px',
+                                       'display': 'block' }));
+
+  wvmpCheck.change(function(evt) {
+    if (wvmpCheck.attr('checked') === 'checked') {
+      if (window.webkitNotifications.checkPermission() !== 0) {
+        window.webkitNotifications.requestPermission();
+      }
+      doNotify = true;
+    }
+    else {
+      doNotify = false;
     }
     evt.preventDefault();
   });
 
+  if (window.webkitNotifications.checkPermission() === 0) {
+    // user has allowed us
+    wvmpCheck.attr('checked', 'checked');
+    doNotify = true;
+  }
+
   // append <p><a>Get notifications...views your profile!</a></p>
-  wvmpModule.children('.content').append($('<p>').append(link));
+  wvmpModule.children('.content').append(wvmpSetting);
+
+  //showNotification('http://static02.linkedin.com/scds/common/u/img/icon/icon_no_photo_40x40.png', 'Gordon Koo', 'http://www.linkedin.com/profile/view?id=34439388&trk=tab_pro');
 
   setInterval(function() {
-    $.get('http://www.linkedin.com/v1/people/~/profile-statistics/profile-views', function(data) {
+    if (!doNotify || window.webkitNotifications.checkPermission() !== 0) { return; }
+    $.get(LI_API_PREFIX + '~/profile-statistics/profile-views', function(data) {
       var views = $(data).find('profile-view');
 
       views.each(function(i, view) {
